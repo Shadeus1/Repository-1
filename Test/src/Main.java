2add1d1a7c20b2ea3f33d4f99d437b14b79c540d
Geissler <geissler_1@hotmail.com>
2024-04-26 18:20:34 +0200

    Animal sounds

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -1,5 +1,33 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Main {
+    private static HashMap<String, String> animalSounds = new HashMap<>();
+
+    static {
+        animalSounds.put("Dog", "Woof");
+        animalSounds.put("Cat", "Meow");
+        animalSounds.put("Mouse", "Squeak");
+        animalSounds.put("Cow", "Mooo");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ArrayList<String> animals = new ArrayList<>();
+        animals.add("Dog");
+        animals.add("Cat");
+        animals.add("Mouse");
+        animals.add("Cow");
+
+        for (String animal : animals) {
+            if (animal.equals("Dog"))  {
+                continue;
+            }
+            System.out.println(animal + " says " + getSound(animal));
+        }
+    }
+
+    private static String getSound(String animal) {
+        return animalSounds.get(animal);
     }
 }
